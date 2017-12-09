39e6708d33bee7cb1f83bd9aa1fb3031dc065292
Obed Ademang <obed.ademang@gmail.com>
2017-12-09 02:05:45 +0000

    Added dotenv to conform to 12-factor requirement

--- src/config.php
+++ src/config.php
@@ -1,15 +1,18 @@
 <?php
 
+$dotenv = new DotEnv\DotEnv(__DIR__);
+$dotenv->load();
+
 return [
     "settings" => [
         "displayErrorDetails" => true,
         "addContentLengthHeader" => false,
         "db" => [
-            "type" => "mysql",
-            "host" => "localhost",
-            "name" => "slimapp",
-            "user" => "root",
-            "passwd" => "wfBAT@gh"
+            "type" => $_ENV['DB_TYPE'],
+            "host" => $_ENV['DB_HOST'],
+            "name" => $_ENV['DB_NAME'],
+            "user" => $_ENV['DB_USER'],
+            "passwd" => $_ENV['DB_PASS']
         ]
     ]
 ];
